aea1abc819635fefdaf6ca50be37b67f0bf78d4a
ISouthRain <isouthrain@gmail.com>
2022-06-28 20:08:12 +0800

    Update README

--- core/init-system.el
+++ core/init-system.el
@@ -19,9 +19,27 @@
 ;;     )
 
 ;; 设置编码
-(setq default-buffer-file-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+;; (setq default-buffer-file-coding-system 'utf-8)
+;; (prefer-coding-system 'utf-8)
+(set-language-environment 'Chinese-GB) ;; 设置为中文简体语言环境
+(set-keyboard-coding-system 'utf-8)    ;; 设置键盘输入时的字符编码
 
+;; set coding config, last is highest priority.
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Recognize-Coding.html#Recognize-Coding
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+(prefer-coding-system 'utf-16)
+(prefer-coding-system 'utf-8-dos)
+(prefer-coding-system 'utf-8-unix)
+;; 文件默认保存为 utf-8
+(set-buffer-file-coding-system 'utf-8-unix)
+(set-default buffer-file-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+;; 解决文件目录的中文名乱码
+(setq-default pathname-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
 
 ;; 系统区分
 (use-package subr-x
