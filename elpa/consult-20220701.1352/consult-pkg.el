85ccc6e24059d947055069c8c2060dac0d025027
ISouthRain <isouthrain@gmail.com>
2022-07-03 12:19:56 +0800

    Add elpa

---
+++ elpa/consult-20220701.1352/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20220701.1352" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "28.1"))
+  :commit "ef4b83cf7d1538840a698fd8e44fcd1e6fdeeede" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
