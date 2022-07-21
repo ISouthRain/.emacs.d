dd4207ccaf516911297706f9ba44a783bf40ce4a
ISouthRain <65887663+ISouthRain@users.noreply.github.com>
2022-07-21 10:19:09 +0800

    Remove elpa

--- elpa/evil-nerd-commenter-3.5.7/evil-nerd-commenter-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from evil-nerd-commenter.el  -*- no-byte-compile: t -*-
-(define-package "evil-nerd-commenter" "3.5.7" "Comment/uncomment lines efficiently. Like Nerd Commenter in Vim" '((emacs "25.1")) :commit "29ced6fda6a76771a8a054ef55c13a1330793d4d" :authors '(("Chen Bin" . "chenbin.sh@gmail.com")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :keywords '("convenience" "evil") :url "http://github.com/redguardtoo/evil-nerd-commenter")
