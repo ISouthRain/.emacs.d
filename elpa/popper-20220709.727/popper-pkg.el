8af1322e7e130e427b638ae841a5330dc30b6809
ISouthRain <isouthrain>
2022-07-21 10:16:21 +0800

    Remove elpa

---
+++ elpa/popper-20220709.727/popper-pkg.el
@@ -0,0 +1,12 @@
+(define-package "popper" "20220709.727" "Summon and dismiss buffers as popups"
+  '((emacs "26.1"))
+  :commit "3406efb1df99eb0d9469b966a0590c5b6ff949d1" :authors
+  '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com"))
+  :maintainer
+  '("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")
+  :keywords
+  '("convenience")
+  :url "https://github.com/karthink/popper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
